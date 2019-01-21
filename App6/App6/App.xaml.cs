a5ad192e7532ba8f0967a98709f125553389ae23
dajkam <sopotfilip@onet.pl>
2019-01-21 21:52:59 +0100

    projekt prawie gotowy

---
+++ App6/App6/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace App6
+{
+	public partial class App : Application
+	{
+		public EditInventoryPage EditInventoryPage { get; private set; }
+
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new MainPage();
+			EditInventoryPage = new EditInventoryPage();
+
+			MainPage = new NavigationPage(new EditInventoryPage());
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ App6/App6/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App6
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+		private void onClick(object sender, EventArgs e)
+		{
+			(sender as Button).Text = "I was just clicked!";
+			//Navigation.PushAsync(new EditInventoryPage());
+		}
+
+		private void onPress(object sender, EventArgs e)
+		{
+			(sender as Button).Text = "You pressed me!";
+			Navigation.PushAsync(new EditInventoryPage());
+		}
+	}
+}
