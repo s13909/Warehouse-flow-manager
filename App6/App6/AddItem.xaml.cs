328a5555920b5421d58eb2652bfb84a6cb98f0fb
dajkam <sopotfilip@onet.pl>
2019-01-22 13:00:32 +0100

    prawie działa

---
+++ App6/App6/AddItem.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App6
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AddItem : ContentPage
+
+		
+	{
+		public StackLayout loggerLayout = new StackLayout();
+		 public Entry  name, value;
+		public Button sub;
+		public AddItem ()
+		{
+
+			name = new Entry
+			{
+				Placeholder = "Name"
+			};
+
+			value = new Entry
+			{
+				Placeholder = "value"
+			};
+
+			sub = new Button
+			{
+				Text = "Submit"
+
+			};
+
+			loggerLayout.Children.Add(name);
+			loggerLayout.Children.Add(value);
+			loggerLayout.Children.Add(sub);
+			InitializeComponent ();
+		}
+	}
+}
